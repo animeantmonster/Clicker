469494b5d1231b88b241eaaa26fc78eefb3f7291
animeantmonster <yurybaltsev@gmail.com>
2025-04-09 21:05:48 +0300

    Add files via upload

---
+++ JS/Shop.js
@@ -0,0 +1,5 @@
+document.addEventListener('DOMContentLoaded', () => {
+    document.getElementById('shop-btn').addEventListener('click', () => {
+        console.log('Shop button clicked');
+    });
+});
