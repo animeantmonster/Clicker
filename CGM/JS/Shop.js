376c09213977af634700fe889d897e47b0419a50
animeantmonster <yurybaltsev@gmail.com>
2025-04-09 21:05:22 +0300

    Delete CGM directory

--- CGM/JS/Shop.js
+++
@@ -1,5 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-    document.getElementById('shop-btn').addEventListener('click', () => {
-        console.log('Shop button clicked');
-    });
-});
